7348a24e4cb10a6ac42bb59322e2133fd2adfda8
kiyell <kysoftware@gmail.com>
2015-12-31 12:21:58 -0500

    Added Activities

---
+++ app/src/main/java/beetrootplanner/utility/kiyell/com/beetrootplanner/DetailActivity.java
@@ -0,0 +1,12 @@
+package beetrootplanner.utility.kiyell.com.beetrootplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
